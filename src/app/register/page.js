3bf969ce1e764e17089575233626c73fb52d1c00
Thiago Fontanella <thiago.fontanella@hotmail.com>
2024-01-05 16:26:25 -0300

    feature: finish login page layout and create register form

---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/header/header";
+import RegisterForm from "@/components/registerForm/registerForm";
+
+export default function Register() {
+    return (
+       <>
+        <Header />
+        <RegisterForm />
+       </>
+    )
+}
--- src/components/loginForm/loginForm.tsx
+++ src/components/loginForm/loginForm.tsx
@@ -14,10 +14,14 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Link from 'next/link'
 
 const formSchema = z.object({
     username: z.string().min(2, {
         message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters.",
     }),
 })
 
@@ -27,6 +31,7 @@ export default function LoginForm() {
         resolver: zodResolver(formSchema),
         defaultValues: {
           username: "",
+          password: ""
         },
     })
 
@@ -38,26 +43,46 @@ export default function LoginForm() {
   }
     
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-                <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-                </FormItem>
-            )}
-            />
-            <Button type="submit">Submit</Button>
-        </form>
-        </Form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-2/5 bg-antiFlashWhite p-12 rounded-md shadow-lg mx-auto mt-28">
+                    <h1 className="text-2xl font-bold">Entrar</h1>
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Usuário</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="shadcn" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public display name.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Senha</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="***" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your password.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div>
+                        <Link href="/register">Não tenho cadastro.</Link>
+                    </div>
+                    <Button type="submit">Entrar</Button>
+                </form>
+            </Form>
     )
 }
